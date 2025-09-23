23f1cc5af39b733393ebfae8b1ce6865d5d7b90b
philiyonas <philiyonas13@gmail.com>
2025-09-23 22:23:42 +0300

    api modelsling the note object

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+"""expalanation of class Note: it represents a note-taking model with fields for title, 
+content, creation timestamp, and an author relationship to the User model."""
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('User',on_delete=models.CASCADE,related_name='notes')# Foreign key to User model for author relationship
+    
+    def __str__(self):
+        return self.title
---
+++ backend/api/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer): # Serializer for User model created implecitly with fields id, username, password    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}# Ensure password is write-only
+    def create(self, validated_data): # Override create method to hash password
+        user =User.objects.create_user(**validated_data) # create_user method hashes the password
+        return user
