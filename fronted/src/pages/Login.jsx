865c61ac059bd6f24f1151f30de39e34ae7fcf8b
philiyonas <philiyonas13@gmail.com>
2025-09-24 20:49:32 +0300

    home login register pages jsx layout

---
+++ fronted/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+function login() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>Please log in to access NOTES/protected routes.</p>
+        </div>
+    );
+}
---
+++ fronted/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+function Register() {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <p>Please register to create an account.</p>
+        </div>
+    );
+}
