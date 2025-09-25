41f9dfaaca7c4c853b9d2bc131ec3f5d539df448
philiyonas <philiyonas13@gmail.com>
2025-09-25 05:00:34 +0300

    successful  communication b/n front end and backed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import Form from "../components/Form"
 
 function Register() {
-    return <Form route="/api/user/register/" method="register" /> // using Form component with route request and method props to specify registration form
+    return <Form route="/api/user/register/" method="register"/>
 }
+
 export default Register
